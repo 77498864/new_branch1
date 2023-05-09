24ebf600525288568862029b4632d95a930a6fb4
77498864 <aa28801136@gmail.com>
2023-05-09 19:53:47 +0800

    all-function

---
+++ add1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+template <class T>
+T add(T a,T b){
+	return a+b;
+}
+template <class T>
+T sub(T a,T b){
+	return b-a;
+}
+
+template <class T>
+T mul(T a,T b){
+	return b*a;
+}
+
+template <class T>
+T div(T a,T b){
+	return b/a;
+}
+
+int main(){
+	double a=10,b=15;
+	cout<<add(a,b)<<endl;
+	cout<<sub(a,b)<<endl; 
+	cout<<mul(a,b)<<endl; 
+	cout<<div(a,b)<<endl; 
+	return 0;
+}
